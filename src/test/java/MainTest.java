aaa420505c1d270da67255737167dd11efcff8f2
Pussa <45294408+Pussa@users.noreply.github.com>
2021-01-17 14:43:48 +0500

    second commit

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,6 +1,8 @@
 import org.testng.annotations.Test;
-import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static io.restassured.path.json.JsonPath.from;
@@ -10,7 +12,7 @@ public class MainTest {
     String response;
     List<String> facts;
     List<String> users;
-    int number_of_ks;
+    String mvp;
 
     @Test
     public void test() {
@@ -18,10 +20,13 @@ public class MainTest {
         response = get("https://cat-fact.herokuapp.com/facts").asString();
         facts = from(response).getList("findAll{it.deleted == false}.text");
         users = from(response).getList("findAll{it.deleted == false}.user");
-        number_of_ks = Collections.frequency(users, "58e007480aac31001185ecef");
-        if (number_of_ks > users.size() / 2)
-            System.out.println("Kasimir is mvp \n" + facts + "\n" + users);
-        else
-            System.out.println("Kasimir isn't mvp  \n" + facts + "\n" + users);
+        mvp = users.stream()
+                .collect(Collectors.groupingBy(w -> w, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Comparator.comparing(Map.Entry::getValue))
+                .get()
+                .getKey();
+        System.out.println(mvp + "\n" + facts + "\n" + users);
     }
 }
